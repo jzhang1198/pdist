1546ac9f2fe0574c5374b2732a63892bf9e51807
jzhang1198 <jzhang1198@gmail.com>
2024-01-28 14:30:27 -0800

    Massive refactor of the codebase

--- pdist/__init__.py
+++ pdist/__init__.py
@@ -1,18 +1,27 @@
 
-from multiprocessing import Pool
+import multiprocessing
 from Bio import SeqIO
 from tqdm import tqdm
 import numpy as np
 import os 
 
 
+format_dict = {
+    'sto': 'stockholm',
+    'st': 'stockholm',
+    'fa': 'fasta',
+    'fasta': 'fasta',
+    'a2m': 'fasta',
+    'clustal': 'clustal'
+}
+
+
 class MSA:
     def __init__(
             self, 
             file: str, 
-            fmt: str,
-            remove_lowercase: bool = True
-            ):
+            fmt: str                
+        ):
         """ 
         Data class for modeling multiple sequence alignments.
 
@@ -21,22 +30,55 @@ class MSA:
         be toggled by setting remove_lowercase to False.
         """
 
-        seqs, ids = zip(*[(str(r.seq), r.id) for r in SeqIO.parse(file, fmt)])
+        try:
+            seqs, ids = zip(*[(str(r.seq), r.id) for r in SeqIO.parse(file, fmt)])
+            ids = np.array(ids)
+        except ValueError as e:
+            print(f'''Issue parsing the input MSA file. Ensure that you are using a format compatible with BioPython's SeqIO.parse() function.''')
+            raise 
+
         l, w = len(seqs), len(seqs[0])
         seqs = np.array(list(''.join([seq for seq in seqs]))).reshape((l, w))
 
-        # clean the seqs 
-        non_gap_columns = np.where(~np.all(seqs == '-', axis=0))[0]
-        seqs = seqs[:, non_gap_columns]
+        self._raw_seqs = np.copy(seqs)
 
-        # remove unaligned columns
-        if remove_lowercase:
-            unaligned_columns = np.where(~np.any(np.char.islower(seqs), axis=0))[0]
-            seqs = seqs[:, unaligned_columns]
+        # remove columns containing all gaps or unaligned characters
+        unaligned_indices = np.where(np.char.islower(seqs))
+        seqs[unaligned_indices[0], unaligned_indices[1]] = np.array((['?'] * len(unaligned_indices[0])))
+
+        self.unaligned_mask = seqs == '?'
+        self.gap_mask = seqs == '-'
+        self.uncovered_mask = np.all(np.logical_or(self.unaligned_mask, self.gap_mask), axis=0)
+
+        self.seqs, self.ids, self.l = seqs, ids, l
+        self.tokenized_seqs = None
+
+        # attributes for internal testing purposes only
+        self._non_consensus_cols = np.where(self.uncovered_mask)[0]
+        
+    def get_col(self, j: int, raw: bool = True):
+        if raw:
+            return self._raw_seqs[:, j]
         else:
-            seqs = np.char.upper(seqs)
+            return self.seqs[:, j]
 
-        self.seqs, self.ids = seqs, ids
+    def tokenize_seqs(self, tokenizer: dict):
+
+        def map_func(element):
+            return tokenizer.get(element)
+        
+        vectorized_map_func = np.vectorize(map_func)
+        self.tokenized_seqs = vectorized_map_func(self.seqs)
+
+    def get_seqlens(self):
+        return (~self.gap_mask).sum(axis=1)
+
+
+def pwise_distance(args: tuple):
+    seqs, pair_batch, subsmat = args
+    seqpairs = np.stack([seqs[pair_batch[:, 0]], seqs[pair_batch[:, 1]]])
+    pair_distances = subsmat[seqpairs[0], seqpairs[1]].sum(axis=1)
+    return pair_distances
 
 
 class pdist:
@@ -44,90 +86,95 @@ class pdist:
     def __init__(
             self,
             subsmat: np.ndarray,
-            tokenizer: dict, 
             batch_size: int,
-            n_cpus: int,
-            pid: bool
     ):
         
         self.subsmat = subsmat
-        self.tokenizer = tokenizer
         self.batch_size = batch_size
-        self.n_cpus = n_cpus
-        self.pid = pid
-        self.gap_token_idx = tokenizer.get('-')
-
-    @staticmethod
-    def _pdist(seqs: np.ndarray, pair_batch: np.ndarray, subsmat: np.ndarray):
-        seqpairs = np.stack([seqs[pair_batch[:, 0]], seqs[pair_batch[:, 1]]])
-        pair_distances = subsmat[seqpairs[0], seqpairs[1]].sum(axis=1)
-        return pair_distances
-
-    @staticmethod
-    def _pdist_pid(seqs: np.ndarray, pair_batch: np.ndarray, subsmat: np.ndarray, gap_token_idx: int):
-        seqpairs = np.stack([seqs[pair_batch[:, 0]], seqs[pair_batch[:, 1]]])
-        seqlens = np.min((seqpairs != gap_token_idx).sum(axis=2), axis=0)
-        pair_n_ids = subsmat[seqpairs[0], seqpairs[1]].sum(axis=1)
-        return pair_n_ids / seqlens
-
 
     def __call__(self, seqs: np.ndarray, pairs: np.ndarray, verbose: bool):
 
-        # tokenize seqs
-        def map_func(element):
-            return self.tokenizer.get(element)
-        vectorized_map_func = np.vectorize(map_func)
-        seqs = vectorized_map_func(seqs)
-
-        log_str = 'BLOSUM similarities' if not self.pid else 'sequence identities'
         pair_batches = [pairs[i:i + int(self.batch_size), :] for i in range(0, len(pairs), int(self.batch_size))]
-        pair_batches = tqdm(pair_batches, desc='Computing pairwise {}.'.format(log_str), unit='batch') if verbose else pairs
         
-        if self.n_cpus == 1:
-            distances = []
-            for pair_batch in pair_batches:
-
-                if not self.pid:
-                    distances.append(pdist._pdist(seqs, pair_batch, self.subsmat)) 
-
-                else:
-                    distances.append(pdist._pdist_pid(seqs, pair_batch, self.subsmat, self.gap_token_idx))
-
-        else:
-            with Pool(self.n_cpus) as pool:
-
-                if not self.pid:
-                    args = [(s,b,m) for s, b, m in zip(list(pair_batch), )]
-
-                else:
-                    args = [() ]
-
-                pass
-            pass
+        distances = []
+        pair_batches = tqdm(pair_batches, desc='Computing pairwise distances.', unit='batch') if verbose else pair_batches
+        for pair_batch in pair_batches:
+            distances.append(pwise_distance((seqs, pair_batch, self.subsmat))) 
 
         distances = np.hstack(distances)
         weighted_adj_list = np.hstack([pairs, distances[:, np.newaxis]])
         return weighted_adj_list
+    
+    
+class pdistMultiprocess(pdist):
+    """ 
+    Child pdist class for parallelizing pairwise distance calculations across many CPUs on the same machine.
+    """
+
+    def __init__(
+            self, 
+            subsmat: np.ndarray,
+            batch_size: int,
+            n_cpus: int
+            ):
+        
+        super().__init__(subsmat, batch_size)
+        self.n_cpus = n_cpus
+
+    def __call__(self, seqs: np.ndarray, pairs: np.ndarray, verbose: bool):
+
+        pair_batches = [pairs[i:i + int(self.batch_size), :] for i in range(0, len(pairs), int(self.batch_size))]
+
+        pool = multiprocessing.Pool(self.n_cpus)
+        args = [(seqs, batch, self.subsmat) for batch in pair_batches]
+        if verbose:
+            distances = list(tqdm(pool.imap(pwise_distance, args)))
+
+        else:
+            distances = list(pool.imap(pwise_distance, args))
+            
+        distances = np.hstack(distances)
+        weighted_adj_list = np.hstack([pairs, distances[:, np.newaxis]])
+        return weighted_adj_list
+    
+    
+def get_subsmat_dir():
+    root_dir = os.path.dirname(os.path.abspath(__file__))
+    subsmat_dir = os.path.join(os.path.dirname(root_dir), 'subsmats')
+    return subsmat_dir
 
 
 def read_subsmat(subsmat: str):
 
-    root_dir = os.path.dirname(os.path.abspath(__file__))
+    root_dir = '/Users/jonathanzhang/Documents/code/scripts/pdist/pdist'
+    # root_dir = os.path.dirname(os.path.abspath(__file__))
     subsmat_dir = os.path.join(os.path.dirname(root_dir), 'subsmats')
     available_subsmats = os.listdir(subsmat_dir)
 
     if '{}.npz'.format(subsmat) not in available_subsmats:
         raise Exception('ERROR: {} is not a valid substitution matrix. Ensure that your substitution matrix is saved as an npz file, per the instructions in the README. Available substitution matrices are: {}'.format(subsmat, ', '.join(available_subsmats)))
 
-    subsmat = os.path.join(subsmat_dir, '{}.npz'.join())
+    subsmat = os.path.join(subsmat_dir, '{}.npz'.format(subsmat))
     subsmat_data = np.load(subsmat)
 
     if 'alphabet' not in subsmat_data.keys() or 'subsmat' not in subsmat_data.keys():
         raise Exception('''ERROR: {} is not a valid substitution matrix. Ensure that your npz file includes 'alphabet' and 'subsmat' arrays, per the instructions in the README.''')
 
-    tok2index = {tok:i for i, tok in enumerate(subsmat_data['alphabet'])}
+    subsmat, alphabet = subsmat_data['subsmat'], subsmat_data['alphabet']
 
-    return subsmat_data['subsmat'], tok2index
+    if '-' not in alphabet:
+        alphabet =  np.append(alphabet, '-')
+        subsmat = np.vstack([subsmat, np.zeros(len(subsmat))])
+        subsmat = np.hstack([subsmat, np.zeros(len(subsmat))[:, np.newaxis]])
+
+    if '?' not in alphabet:
+        alphabet =  np.append(alphabet, '?')
+        subsmat = np.vstack([subsmat, np.zeros(len(subsmat))])
+        subsmat = np.hstack([subsmat, np.zeros(len(subsmat))[:, np.newaxis]])
+
+    tok2index = {tok:i for i, tok in enumerate(alphabet)}
+
+    return subsmat, tok2index
 
 
 def distribute_pairs(l: int, task_id: int, n_jobs: int):
@@ -137,6 +184,10 @@ def distribute_pairs(l: int, task_id: int, n_jobs: int):
 
 
 def reformat_esl_output(esl_output, ids: np.ndarray):
+    """ 
+    Deprecated function for re-formatting output from esl-alipid (a miniapp
+    from the hmmer) into a matrix.
+    """
 
     lines = esl_output.stdout.split('\n')[1:-1]
 
@@ -154,47 +205,4 @@ def reformat_esl_output(esl_output, ids: np.ndarray):
     pid_matrix = pid_matrix + pid_matrix.T - np.diag(np.diag(pid_matrix))
     np.fill_diagonal(pid_matrix, 1)
     return pid_matrix
-
-
-def compute_pwise_identity(ali_arr: np.ndarray, tok2index: dict, batch_size=1e6):
-    """ 
-    Deprecated function. Now using esl-alipid, which is much more efficient.
-    """
-
-    pair_indices = np.vstack(np.tril_indices(len(ali_arr), k=-1)).T
-    pid_matrix = np.zeros((len(ali_arr), len(ali_arr)))
-    mlens_matrix = np.zeros((len(ali_arr), len(ali_arr)))
-    match_matrix = np.zeros((len(ali_arr), len(ali_arr)))
-
-    # obtain mapping between index and sequence length
-    seqlens = ali_arr.shape[1] - np.sum(ali_arr == '-', axis=1)
-    ind2seqlen = dict([(i,l) for i,l in enumerate(seqlens)])
-    def length_map_func(element):
-        return ind2seqlen.get(element)
-    vectorized_length_map = np.vectorize(length_map_func)
-
-    ali_arr = clean_ali(ali_arr, remove_lowercase=False)
-    def map_func(element):
-        return tok2index.get(element)
-    vectorized_map_func = np.vectorize(map_func)
-    tokenized_ali = vectorized_map_func(ali_arr)
-    
-    gap_token = tok2index['-']
-
-    for pair_batch in tqdm([pair_indices[i:i + int(batch_size), :] for i in range(0, len(pair_indices), int(batch_size))], desc='Computing pairwise sequence identities.', unit='batch'):
-
-        # Compute the similarity matrix using broadcasting
-        seq1 = tokenized_ali[pair_batch[:, 0]]
-        seq2 = tokenized_ali[pair_batch[:, 1]]
-
-        min_lens = np.vstack([vectorized_length_map(pair_batch[:, 0]), vectorized_length_map(pair_batch[:, 1])]).min(axis=0)
-        pair_identities = np.sum((seq1 == seq2) & (seq1 != gap_token) & (seq2 != gap_token), axis=1) / min_lens
-        pid_matrix[pair_batch[:,1], pair_batch[:,0]] = pair_identities
-        mlens_matrix[pair_batch[:,1], pair_batch[:,0]] = min_lens
-        match_matrix[pair_batch[:,1], pair_batch[:,0]] = np.sum((seq1 == seq2) & (seq1 != gap_token) & (seq2 != gap_token), axis=1)
-
-    pid_matrix = pid_matrix + pid_matrix.T - np.diag(np.diag(pid_matrix))
-    np.fill_diagonal(pid_matrix, 1)
-    
-    return pid_matrix, mlens_matrix, match_matrix
 
